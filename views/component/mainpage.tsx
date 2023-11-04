b440a313a92ba90c33de7bc8fc7e421106c7d4ed
SwitchBoardSoftwareGroup <mattar@colgate.edu>
2023-11-04 02:08:47 -0400

    main page emptied but image not showing

--- views/component/mainpage.tsx
+++ views/component/mainpage.tsx
@@ -1,5 +1,10 @@
 import * as elements from 'typed-html';
-
+// img {
+//   display: block;
+//   margin-left: auto;
+//   margin-right: auto;
+//   width: 50%;
+// }
 
 export const mainPage = <div>
 <header id="header" class="fixed-top  header-transparent ">
@@ -19,13 +24,7 @@ export const mainPage = <div>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
+             
               </li>
               <li><a href="#">Drop Down 2</a></li>
               <li><a href="#">Drop Down 3</a></li>
@@ -47,18 +46,12 @@ export const mainPage = <div>
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
-          <div>
-            <h1>App landing page template</h1>
-            <h2>Lorem ipsum dolor sit amet, tota senserit percipitur ius ut, usu et fastidii forensibus voluptatibus. His ei nihil feugait</h2>
-            <a href="#" class="download-btn"><i class="bx bxl-play-store"></i> Google Play</a>
-            <a href="#" class="download-btn"><i class="bx bxl-apple"></i> App Store</a>
-          </div>
-        </div>
-        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
-          <img src="assets/img/hero-img.png" class="img-fluid" alt=""/>
+           <img src = "public/assets/img/renaisaance_man_logo copy.jpg" alt = "renaissance man"></img>
         </div>
       </div>
     </div>
 
   </section>
   </div>
+
+
