d18f06d23c69bb74af2073593782859be71d7b12
SheltonSS <48075441+SheltonSS@users.noreply.github.com>
2023-11-05 07:45:35 -0500

    Update taskinfo.tsx
    formating fixes

--- views/component/taskinfo.tsx
+++ views/component/taskinfo.tsx
@@ -2,14 +2,6 @@ import * as elements from 'typed-html';
 
 export const taskinfo =
     <div>
-      {/* <div class="col-12"> */}
-        {/* <label for="taskname" class="form-label">Task Name</label> */}
-        {/* <input type="text" class="form-control" id="taskname" placeholder="" value="" required="" name='taskname'/> */}
-        {/* <div class="invalid-feedback">
-          Valid task name is required.
-        </div> */}
-      {/* </div> */}
-
       <div class="col-12">
         <label for="taskdescription" class="form-label">Brief Description</label>
         <input type="text" class="form-control" id="taskdescription" placeholder="" value="" required="" name='taskdescription'/>
